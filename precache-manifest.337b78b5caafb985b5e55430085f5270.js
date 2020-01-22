0cbe3971835d34c239cfa64ecacbeb29368d193f
Mohamadhassan <emohamadhassan@gmail.com>
2020-01-22 13:08:05 +0330

    Updates

--- precache-manifest.337b78b5caafb985b5e55430085f5270.js
+++ precache-manifest.337b78b5caafb985b5e55430085f5270.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa7e08aa5fefc2916d12411f82d5c83c",
+    "revision": "37db5dfb3f08ed1f4d510bfbccdfbada",
     "url": "/ai-maze-solver/index.html"
   },
   {
-    "revision": "06bfcc0078a7e4e47560",
+    "revision": "ab574c5dfac8704cb60f",
     "url": "/ai-maze-solver/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "6b301cb4f7615fd4d98d",
-    "url": "/ai-maze-solver/static/js/2.e3647c57.chunk.js"
+    "revision": "0a577d1cb67364166859",
+    "url": "/ai-maze-solver/static/js/2.c77039cd.chunk.js"
   },
   {
     "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
-    "url": "/ai-maze-solver/static/js/2.e3647c57.chunk.js.LICENSE"
+    "url": "/ai-maze-solver/static/js/2.c77039cd.chunk.js.LICENSE"
   },
   {
-    "revision": "06bfcc0078a7e4e47560",
-    "url": "/ai-maze-solver/static/js/main.50b9b918.chunk.js"
+    "revision": "ab574c5dfac8704cb60f",
+    "url": "/ai-maze-solver/static/js/main.81a390e0.chunk.js"
   },
   {
     "revision": "edcc9f4156de5a51c96f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ai-maze-solver/precache-manifest.1fb2211b731e7947519b055d014fa25e.js"
+  "/ai-maze-solver/precache-manifest.337b78b5caafb985b5e55430085f5270.js"
 );
 
 self.addEventListener('message', (event) => {
